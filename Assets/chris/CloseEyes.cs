7d67a5e99a6b5702342c1a62782b443d0cdabf30
46chris <chris01wy@gmail.com>
2020-10-13 23:40:47 -0700

    Added Semi-working blinking

---
+++ Assets/chris/CloseEyes.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloseEyes : MonoBehaviour
+{
+    //Lid Objects 
+    GameObject TopLid;
+    GameObject BotLid; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TopLid = GameObject.Find("TopEyeL");
+        BotLid = GameObject.Find("BotEyeL");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Eyelid Close on Spacebar
+        if (Input.GetKey(KeyCode.Space))
+        {
+            TopLid.GetComponent<Animator>().SetTrigger("CloseTop");
+            BotLid.GetComponent<Animator>().SetTrigger("CloseBottom");
+        }
+        else
+        {
+            BotLid.GetComponent<Animator>().SetTrigger("OpenBottom");
+            TopLid.GetComponent<Animator>().SetTrigger("OpenTop");
+        }
+    }
+}

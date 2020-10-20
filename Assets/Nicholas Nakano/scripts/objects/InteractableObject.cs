71a62f02a2b7908634564b7f34a507cceb3eaef6
Nicholas Nakano <nicksnak01@gmail.com>
2020-10-20 01:50:26 -0700

    Added object/item interaction system, inventory system, and door system.

---
+++ Assets/Nicholas Nakano/scripts/objects/InteractableObject.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum InteractiveTypes
+{
+    Default,
+    Door,
+    Item
+}
+public class InteractableObject : MonoBehaviour
+{
+    protected string objName;
+    public string interactText;
+
+    public virtual void Interact()
+    {
+        
+    }
+    public virtual void Interact(GameObject obj)
+    {
+
+    }
+}

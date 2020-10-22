d7dd45135970f17cea1c739063e56028dbc82639
Leon Yang <ley015@ucsd.edu>
2020-10-21 22:35:35 -0700

    Started linked paths for AI

--- Assets/Leon/MonsterAI.cs
+++ Assets/Leon/MonsterAI.cs
@@ -15,6 +15,8 @@ public class MonsterAI : MonoBehaviour
     public float wait = 0f;
     // private bool alert = false;
     // private float alertness = 20f;
+    private bool spawn = true;
+    private int pathPicker;
 
     void Start()
     {
@@ -28,8 +30,21 @@ public class MonsterAI : MonoBehaviour
             if (state == "default")
             {
                 // pick one of the points of destinations array
-                dest = Random.Range(0, destinations.Length);
-                nav.SetDestination(destinations[dest].position);
+                // dest = Random.Range(0, destinations.Length);
+                // nav.SetDestination(destinations[dest].position);
+
+                if (spawn)
+                {
+                    nav.SetDestination(destinations[0].position);
+                    spawn = false;
+                    dest = 0;
+                }
+                else
+                {
+                    pathPicker = Random.Range(0, 2);
+                    PickPath(pathPicker);
+                }
+
                 state = "walk";
             }
             if (state == "walk")
@@ -56,6 +71,44 @@ public class MonsterAI : MonoBehaviour
             {
                 nav.destination = player.transform.position;
                 // when hunting time is over, return to default state
+            }
+        }
+    }
+
+    private void PickPath(int picker)
+    {
+        if (picker == 0)
+        {
+            print("going to previous");
+            if (destinations[dest].GetComponent<LinkedDest>().GetPrev())
+            {
+                nav.SetDestination(destinations[dest].GetComponent<LinkedDest>().GetPrev().position);
+
+                // find current pos in list
+                for (int i = 0; i < destinations.Length; i++)
+                {
+                    if (destinations[i].position == destinations[dest].GetComponent<LinkedDest>().GetPrev().position)
+                    {
+                        dest = i;
+                    }
+                }
+            }
+        }
+        if (picker == 1)
+        {
+            print("going to next");
+            if (destinations[dest].GetComponent<LinkedDest>().GetNext())
+            {
+                nav.SetDestination(destinations[dest].GetComponent<LinkedDest>().GetNext().position);
+
+                // find current pos in list
+                for (int i = 0; i < destinations.Length; i++)
+                {
+                    if (destinations[i].position == destinations[dest].GetComponent<LinkedDest>().GetNext().position)
+                    {
+                        dest = i;
+                    }
+                }
             }
         }
     }

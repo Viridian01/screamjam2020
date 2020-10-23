e23e14adbd74499d24c7c01dff1dff73d9f1ef5a
mattlawr <mattblawr@gmail.com>
2020-10-22 23:59:37 -0700

    Added level textures and computer sounds

---
+++ Assets/matthew/AudioLoopOffset.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioLoopOffset : MonoBehaviour
+{
+    public AudioSource source;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!source || !source.clip) return;
+
+        source.PlayDelayed(Random.Range(0f, source.clip.length/2f));
+    }
+}

43010651df971763b2a1c9cf597bbd1b37a30093
Nicholas Nakano <nicksnak01@gmail.com>
2020-10-23 13:53:21 -0700

    Added new font assets alongside narration text.

---
+++ Assets/NarrationText.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class NarrationText : MonoBehaviour
+{
+
+    TextMeshProUGUI associatedText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        associatedText = GetComponent<TextMeshProUGUI>();
+    }
+
+    public void DisplayText(string textToShow, float timeToWait)
+    {
+        StartCoroutine(TimedText(textToShow, timeToWait));
+    }
+
+    IEnumerator TimedText(string text, float time)
+    {
+        associatedText.text = text;
+        yield return new WaitForSeconds(time);
+        associatedText.text = "";
+        StopCoroutine(TimedText(text, time));
+    }
+}

230f928921199cbe65a11ceab0475683073f40aa
Nicholas Nakano <nicksnak01@gmail.com>
2020-10-23 14:10:04 -0700

    Added final door.

---
+++ Assets/FinalDoor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinalDoor : InteractableObject
+{
+    private NarrationText narrationText;
+
+    void Awake()
+    {
+        objName = "Door";
+        interactText = "Escape this nightmare.";
+        narrationText = FindObjectOfType<NarrationText>();
+    }
+
+    public override void Interact(GameObject user)
+    {
+        base.Interact(user);
+        narrationText.DisplayText("Office Hours are over.", 5);
+    }
+}
